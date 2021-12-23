09b8d9ee818df5c15c6b08716a296fc9a3155135
ngchisern <ncsern2000@gmail.com>
2021-12-23 14:19:36 +0800

    Modify axious URL for deployment

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5001/api/v1/",
+  baseURL: "https://heh-heh-generator.herokuapp.com/api/v1/",
   headers: {
     "Content-type": "application/json",
   },
